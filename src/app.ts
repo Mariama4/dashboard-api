78a38218a8d485ba8302125722e442250d485699
mariama4 <mariama4@mail.ru>
2023-03-27 23:14:13 +0700

    upload study project

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import { IUserController } from './users/user.controller.interface';
+import express, { Express } from 'express';
+import { Server } from 'http';
+import { inject, injectable } from 'inversify';
+import { IExceptionFilter } from './errors/exception.filter.interface';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import 'reflect-metadata';
+import { json } from 'body-parser';
+import { IConfigService } from './config/config.service.interface';
+import { UserController } from './users/user.controller';
+import { PrismaService } from './database/prisma.service';
+import { AuthMiddleware } from './common/auth.middleware';
+
+@injectable()
+export class App {
+	app: Express;
+	port: number;
+	server: Server;
+
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.IUserController) private userController: UserController,
+		@inject(TYPES.IExceptionFilter) private exceptionFilter: IExceptionFilter,
+		@inject(TYPES.IConfigService) private configService: IConfigService,
+		@inject(TYPES.IPrismaService) private prismaService: PrismaService,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
+
+	useMiddleware(): void {
+		this.app.use(json());
+		const authMiddleware = new AuthMiddleware(this.configService.get('SECRET'));
+		this.app.use(authMiddleware.execute.bind(authMiddleware));
+	}
+
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
+	}
+
+	useExceptionFilters(): void {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
+
+	public async init(): Promise<void> {
+		this.useMiddleware();
+		this.useRoutes();
+		this.useExceptionFilters();
+		await this.prismaService.connect();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
+
+	public close(): void {
+		this.server.close();
+	}
+}
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export const TYPES = {
+	Application: Symbol.for('Application'),
+	ILogger: Symbol.for('ILogger'),
+	IUserController: Symbol.for('IUserController'),
+	IUserService: Symbol.for('IUserService'),
+	IExceptionFilter: Symbol.for('IExceptionFilter'),
+	IConfigService: Symbol.for('IConfigService'),
+	IPrismaService: Symbol.for('IPrismaService'),
+	IUsersRepository: Symbol.for('IUsersRepository'),
+};
